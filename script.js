a941114b2c3b95d66292775dd4ae4a379da9750b
AlbertoP231 <aprado1012@gmail.com>
2022-11-03 19:19:56 -0600

    Agregamos Background images para la busqueda

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ let weather = {
         document.querySelector(".wind").innerText 
         = "Velocidad del viento: " + speed + "km/h";
         document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1920x1080/?landscape)" + name + "')"
     },
     search: function (){
         this.fetchWeather(document.querySelector(".search-bar").value);
